f15fadce3837754a4d594c3f997feb45003dd936
Tokiwa-17 <2872444509@qq.com>
2023-01-01 00:06:29 +0800

    lab1 exercise 3

--- fa22-lab-starter/lab01/exercise3/ll_cycle.c
+++ fa22-lab-starter/lab01/exercise3/ll_cycle.c
@@ -3,4 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *p = head;
+    node *q = head;
+    if (!p) return 0;
+    while(1) {
+        p = p -> next;
+        q = q -> next;
+
+        if (q)
+            q = q -> next;
+
+        if (p == q) return 1;
+        if (!q) return 0;
+    }
 }
